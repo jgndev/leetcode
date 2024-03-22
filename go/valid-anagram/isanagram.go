550d03c327e6572496271178ceffcd75897c8ad8
Jeremy Novak <olepunchy@gmail.com>
2024-03-22 15:08:07 -0500

    completed valid anagram

---
+++ go/valid-anagram/isanagram.go
@@ -0,0 +1,20 @@
+package anagram
+
+import "maps"
+
+func isAnagram(s, t string) bool {
+	sMap := charMap(s)
+	tMap := charMap(t)
+
+	return maps.Equal(sMap, tMap)
+}
+
+func charMap(s string) map[string]int {
+	m := make(map[string]int, len(s))
+
+	for _, char := range s {
+		m[string(char)]++
+	}
+
+	return m
+}
