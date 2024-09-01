a19e7e87e3667927744bdbde4c25c1b9fa0f43ba
Jeremy Novak <jgn@jgn.dev>
2024-09-01 17:41:19 -0500

    completed 141 - linked list cycle in c++

--- cpp/linked_list_cycle/src/solution.cpp
+++ cpp/linked_list_cycle/src/solution.cpp
@@ -1,23 +1,23 @@
 #include "solution.h"
 
-bool has_cycle(struct Node *head) {
+bool Solution::has_cycle(Node *head) {
   // empty lists and lists with one node cannot have a cycle
-  if (head == NULL || head->next == NULL) {
+  if (head == nullptr || head->next == nullptr) {
     return false;
   }
 
   // initialize two pointers, slow and fast.
   // 'slow' moves one step at a time.
   // 'fast' moves two steps at a time.
-  struct Node *slow = head;
+  auto slow = head;
   // 'fast' starts on the next node, one step ahead.
-  struct Node *fast = head->next;
+  auto fast = head->next;
 
-  // if there is no cycle, 'fast' will reach the end of the list.
-  // if there *is* a cycle, 'fast' will catch up to 'slow'.
+  // if there *is no cycle*, 'fast' will reach the end of the list.
+  // if there *is a cycle*, 'fast' will catch up to 'slow'.
 
   // continue until 'fast' reaches the end of the list
-  while (fast != NULL && fast->next != NULL) {
+  while (fast != nullptr && fast->next != nullptr) {
     // if 'slow' and 'fast' meet at any node, there is a cycle.
     if (slow == fast) {
       // cycle detected
@@ -30,6 +30,6 @@ bool has_cycle(struct Node *head) {
     fast = fast->next->next;
   }
 
-  // no cycle found
+  // base case, no cycle found
   return false;
 }
